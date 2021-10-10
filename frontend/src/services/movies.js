b4b379387117d7c7828c6a3026bd633d1454c5db
Neil Kapadia <neilkapadia7@gmail.com>
2021-10-10 19:46:56 +0530

    movies services

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -1,5 +1,35 @@
 import axios from 'axios'
 
+export const trendingMovies = () => {
+    try {
+        const res = await axios.get(`${process.env.MOVIESDB_BASE_URL}/trending/all/week?api_key=${process.env.MOVIES_DB_APIKEY}&language=en-US`)
+        return res.data;
+    } catch (err) {
+        console.log(err)
+        return err
+    }
+}
+
+export const originalsMovies = () => {
+    try {
+        const res = await axios.get(`${process.env.MOVIESDB_BASE_URL}/discover/tv?api_key=${process.env.MOVIES_DB_APIKEY}&with_network=213`)
+        return res.data;
+    } catch (err) {
+        console.log(err)
+        return err
+    }
+}
+
+export const topRatedMovies = () => {
+    try {
+        const res = await axios.get(`${process.env.MOVIESDB_BASE_URL}/movie/top_rated?api_key=${process.env.MOVIES_DB_APIKEY}&language=en-US`)
+        return res.data;
+    } catch (err) {
+        console.log(err)
+        return err
+    }
+}
+
 export const addFavourites = async (data) => {
     try {
         const config = {
@@ -8,7 +38,6 @@ export const addFavourites = async (data) => {
             }
         }
         const res = await axios.post('api/favourites/add', data,  config )
-        console.log('Backend Res :::', res)
         return res.data;
             
     } catch (err) {
